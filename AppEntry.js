a0f296647ffe57db32b2f4f7cfb2e2919348fb70
Raphael Cavalcanti <46363242+raphaelcavalcanti0@users.noreply.github.com>
2022-03-02 05:07:17 -0300

    Initial settings: added icons, splash screen and first page

---
+++ AppEntry.js
@@ -0,0 +1,5 @@
+import registerRootComponent from 'expo/build/launch/registerRootComponent';
+
+import Routes from './src/Routes';
+
+registerRootComponent(Routes);
---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import App from "./App";
+
+const Stack = createNativeStackNavigator();
+
+const Routes = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={App} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Routes;
